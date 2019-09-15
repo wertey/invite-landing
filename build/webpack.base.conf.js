2c9837a927f98f7f8fe99103dfc854378d5c5c3e
Dmitry Verteyko <verteyko1990@gmail.com>
2019-09-15 15:37:14 +0300

    jquery scripts

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const fs = require('fs')
+const webpack =  require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
